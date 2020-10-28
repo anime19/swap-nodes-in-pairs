57249afca2ace0e0387e3ab4094fe94b617d6c8c
Animesh Shukla <oneanimesh@gmail.com>
2020-10-28 09:51:16 +0530

    Update solution.java

--- solution.java
+++ solution.java
@@ -10,20 +10,17 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        boolean b= true;
-        ListNode p=head; int t;
-        while( p.next!=null) {
-            if(b){
-                t=p.val;
-                p.val=p.next.val;
-                p.next.val=t;
-                p=p.next;
-            }
-        else p=p.next;
-        b=!b;
-        }
-    
-   return head;
-
+      ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode current = dummy;
+    while (current.next != null && current.next.next != null) {
+        ListNode first = current.next;
+        ListNode second = current.next.next;
+        first.next = second.next;
+        current.next = second;
+        current.next.next = first;
+        current = current.next.next;
+    }
+    return dummy.next;
     }
 }
